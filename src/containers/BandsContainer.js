95662c2d6edd438b8c965bcea764cc41a94176d0
Veenie <raveen08@gmail.com>
2021-04-12 12:33:46 -0700

    Done.

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import Band from './src/components/Band'
 
 class BandsContainer extends Component {
+  
+  renderBand = () => this.props.band.map((band, id) => <Band key={id} text={band} />)
   render() {
     return(
       <div>
         BandsContainer
+        {this.renderBand()}
       </div>
     )
   }
